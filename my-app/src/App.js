02ca0c3363994bfac5775c59e094b729d5a31d5b
wzz <ywc0008@naver.com>
2023-05-15 01:43:23 +0900

    learning react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,19 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [BTC, setBTC] = useState(false);
-  const onClick = () => {
-    setBTC((current) => !current);
-  };
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      {BTC ? (
-        <button onClick={onClick}>USD</button>
-      ) : (
-        <button onClick={onClick}>BTC</button>
-      )}
-      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
