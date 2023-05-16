a224605260a04276e0fd02756e12cf052bb41c1a
wzz <ywc0008@naver.com>
2023-05-16 15:20:12 +0900

    홈페이지 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Movie from "../components/Movie";
 
 function Detail() {
   const [loading, setLoading] = useState(true);
